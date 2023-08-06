a4edefcdd98092345b24f571765a0807a51df812
olhkyle <kylekwon96@gmail.com>
2023-08-06 22:04:39 +0900

    [FEAT] add export index in common components

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,7 @@
 export { default as Nav } from './Nav';
 export { default as Input } from './Input';
+export { default as Button } from './Button';
+export { default as Text } from './Text';
+export { default as Spacer } from './Spacer';
+export { default as Flex } from './Flex';
+export { default as SideNav } from './SideNav';

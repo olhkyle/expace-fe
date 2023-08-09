35e18fb288a4d82889a2abf2c26ff584517ce3aa
olhkyle <kylekwon96@gmail.com>
2023-08-09 18:00:34 +0900

    [FEAT] add export default as ForgotPassword

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,3 +4,4 @@ export { default as SignUp } from './SignUp';
 export { default as Space } from './Space';
 export { default as Reservation } from './Reservation';
 export { default as About } from './About';
+export { default as ForgotPassword } from './ForgotPassword';

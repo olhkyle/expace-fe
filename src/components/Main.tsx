a175f2ecd87b2f204f07b5f1d29332378fd2938c
olhkyle <kylekwon96@gmail.com>
2023-08-09 17:58:44 +0900

    [-] reorder import position

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
-import styled from '@emotion/styled';
 import { HTMLAttributes } from 'react';
+import styled from '@emotion/styled';
 
 interface MainProps extends HTMLAttributes<HTMLElement> {
 	onClick: () => void;

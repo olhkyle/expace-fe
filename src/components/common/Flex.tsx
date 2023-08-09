cef94751608a4ef7b5290ce6e907a303710482cd
olhkyle <kylekwon96@gmail.com>
2023-08-09 18:00:45 +0900

    [FEAT] update Flex Component props

--- src/components/common/Flex.tsx
+++ src/components/common/Flex.tsx
@@ -4,9 +4,22 @@ interface FlexProps extends HTMLAttributes<HTMLDivElement> {
 	direction?: 'row' | 'column';
 	justifyContent?: 'flex-start' | 'flex-end' | 'space-between' | 'center';
 	alignItems?: 'flex-start' | 'flex-end' | 'center';
+	gap?: string;
+	margin?: string;
+	padding?: string;
+	width?: number;
 }
 
-const Flex = ({ direction = 'row', justifyContent = 'flex-start', alignItems = 'center', ...props }: FlexProps) => {
+const Flex = ({
+	direction = 'row',
+	justifyContent = 'flex-start',
+	alignItems = 'center',
+	gap = '0',
+	margin = '0px',
+	padding = '0px',
+	width,
+	...props
+}: FlexProps) => {
 	return (
 		<div
 			css={{
@@ -14,6 +27,10 @@ const Flex = ({ direction = 'row', justifyContent = 'flex-start', alignItems = '
 				flexDirection: direction,
 				justifyContent,
 				alignItems,
+				margin,
+				padding,
+				gap,
+				width: `${width}%`,
 			}}
 			{...props}
 		/>

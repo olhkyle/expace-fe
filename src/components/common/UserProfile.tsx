d0dc82af485723918284f7e82a5c07ff30cf9332
olhkyle <kylekwon96@gmail.com>
2023-09-06 22:34:48 +0900

    [FEAT] add UserProfile Component

---
+++ src/components/common/UserProfile.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const UserProfile = styled.span`
+	display: none;
+	font-weight: 700;
+
+	@media screen and (min-width: 768px) {
+		display: block;
+	}
+`;
+
+export default UserProfile;
